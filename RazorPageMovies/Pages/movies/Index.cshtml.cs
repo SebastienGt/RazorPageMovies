b37b9bda591cee9fb9e9f2a03aa743a02732cc00
Sébastien <sebastien.friedberg@gmail.com>
2021-01-08 19:04:23 +0100

    First commit

---
+++ RazorPageMovies/Pages/movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using RazorPageMovies.Data;
+using RazorPageMovies.modèles;
+
+namespace RazorPageMovies.Pages.movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly RazorPageMovies.Data.RazorPageMoviesContext _context;
+
+        public IndexModel(RazorPageMovies.Data.RazorPageMoviesContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
---
+++ RazorPageMovies/modèles/Movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPageMovies.modèles
+{
+    public class Movie
+    {
+        public int ID { get; set; }
+        public string title { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+    }
+}
